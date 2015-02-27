db7ac747d1ddacb2fddad645bbe66512fe83ce26
vinilzen <marchukilya@gmail.com>
2015-02-27 14:18:03 +0100

    add search layout

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(function(){
+	var supportsOrientationChange = "onorientationchange" in window,
+		orientationEvent = supportsOrientationChange ? "orientationchange" : "resize";
+
+	function setMapHeight(){
+		var wh = $(window).height(),
+			ww = $(window).width();
+
+		if (ww>768){
+			$('#js-map').show().css({
+				height:wh,
+				width:(ww-768)
+			});
+		} else {
+			$('#js-map').hide();
+		}
+	}
+
+	window.addEventListener(orientationEvent, setMapHeight);
+
+	setMapHeight();
+});
